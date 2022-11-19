5b8e60ea0fa634d7a7194b463351fa4107ed096b
Trevor <shawtrevor97@yahoo.com>
2022-11-18 18:26:49 -0800

    attempt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, Request, Depends
+
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
 import uvicorn 
 
 from sqlalchemy.orm import Session 
@@ -23,6 +27,9 @@ app = FastAPI()
 app.include_router(users_r.router)
 app.include_router(posts_r.router)
 
+
+templates = Jinja2Templates(directory='templates/')
+
 # test route 
 def root(): 
     return {
@@ -31,11 +38,8 @@ def root():
     
 # test 
 app.get("/test")
-def test_post(db: Session = Depends(get_db)):
-    return {
-        "message": "Test"
-    }
-
+def test_post(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("create_user.html")
 
 # Start by "python main.py"
 if __name__ == "__main__":
